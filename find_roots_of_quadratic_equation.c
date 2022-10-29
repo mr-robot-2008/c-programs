bd7e529cb2b4baeadfb62f9e077820f59f966d33
Amul Rathore <71003626+amul-code@users.noreply.github.com>
2022-10-29 10:52:20 +0530

    find roots of a quadratic equation
    Finding Roots of a Quadratic Equation in C.

---
+++ find_roots_of_quadratic_equation.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+ 
+void findRoots(int a, int b, int c)
+{
+    if (a == 0) {
+        printf("Invalid");
+        return;
+    }
+ 
+    int d = b * b - 4 * a * c;
+    double sqrt_val = sqrt(abs(d));
+ 
+    if (d > 0) {
+        printf("Roots are real and different \n");
+        printf("%f\n%f", (double)(-b + sqrt_val) / (2 * a),(double)(-b - sqrt_val) / (2 * a));
+    }
+    else if (d == 0) {
+        printf("Roots are real and same \n");
+        printf("%f", -(double)b / (2 * a));
+    }
+    else // d < 0
+    {
+        printf("Roots are complex \n");
+        printf("%f + i%f\n%f - i%f", -(double)b / (2 * a), sqrt_val/(2 * a), -(double)b / (2 * a), sqrt_val/(2 * a));
+    }
+}
+ 
+int main()
+{
+    int a = 1, b = 4, c = 4;
+   
+    findRoots(a, b, c);
+    return 0;
+}
